89ece6a5e40b0f5e7506f9a0c920cce0023a12a5
tcstrength <thai.cuong1404@gmail.com>
2024-11-10 18:47:24 +0700

    rename pending_post_entity to pending_post

--- src/hmd/entity/pending_post_entity.py
+++ src/hmd/entity/pending_post_entity.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String
 from hmd.entity.base_entity import BaseEntity
 
 class PendingPostEntity(BaseEntity):
-    __tablename__ = "pending_post_entity"
+    __tablename__ = "pending_post"
     __table_args__ = {'extend_existing': True}
     post_id = Column(Integer, primary_key=True, autoincrement=False)
     post_url = Column(String(1024))
